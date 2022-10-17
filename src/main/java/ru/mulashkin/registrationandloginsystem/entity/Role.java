93730b12dc3bd31d5394f8864ec23a6f6e9dfc0d
FedorMulashkin <fedor.mulashkin@gmail.com>
2022-10-17 18:03:52 +0300

    Add entities

---
+++ src/main/java/ru/mulashkin/registrationandloginsystem/entity/Role.java
@@ -0,0 +1,23 @@
+package ru.mulashkin.registrationandloginsystem.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ru/mulashkin/registrationandloginsystem/entity/User.java
@@ -0,0 +1,46 @@
+package ru.mulashkin.registrationandloginsystem.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.repository.cdi.Eager;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"
+            )
+    )
+    private Collection<Role> roles;
+
+    public User(String firstName, String lastName, String email, String password, List<Role> roles) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+}
