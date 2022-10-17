fd8cbc12b9926629851785e39a9eafce566fb8fb
FedorMulashkin <fedor.mulashkin@gmail.com>
2022-10-17 17:34:48 +0300

    Add dependencies

---
+++ src/test/java/ru/mulashkin/registrationandloginsystem/RegistrationAndLoginSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mulashkin.registrationandloginsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAndLoginSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
